a907077aefba3cc38375ded35e1490757b0b8185
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-09-30 14:55:24 +0530

    Create string.py

---
+++ LAB 5/strings/string.py
@@ -0,0 +1,10 @@
+str="sai ram"
+print(str.capitalize()) #first letter capital
+print(str.upper())	#whole string is capital 
+print(str[0].isupper()) #boolean isupper() islower()
+print(str.title())	#first letter in every word is capital
+'''
+->isalnum()
+->isalpha()
+->isdigit()
+'''
