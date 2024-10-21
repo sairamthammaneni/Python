09027f9db390259fa852566f1705c4c516721fd9
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-10-21 15:39:34 +0530

    Create Arthmetic.py

---
+++ Week-7/Arthmetic.py
@@ -0,0 +1,12 @@
+def add(a,b) :
+	return a+b
+def sub(a,b) :
+	return a-b
+def mul(a,b) :
+	return a*b
+def div(a,b) :
+	return a//b
+print("Addition : ",add(2,2))
+print("Subtraction : ",sub(7,2))
+print("Multiplication : ",mul(2,3))
+print("division : ",div(14,2))
